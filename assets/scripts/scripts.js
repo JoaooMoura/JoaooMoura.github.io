89eed5e372990963e12981458b6b9191592d3bb5
Joao Moura <jv.moura.sjc@gmail.com>
2025-04-14 09:41:34 -0300

    Incrementação Carrossel

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,31 +1,61 @@
-const toggleTheme = document.getElementById("toggleTheme")
-const rootHtml = document.documentElement
-const accordionHeaders = document.querySelectorAll(".accordion__header")
-const menuLinks = document.querySelectorAll(".menu__link")
 
-function changeTheme(){
-    const currentTheme = rootHtml.getAttribute("data-theme")
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
 
-    if(currentTheme === "dark") rootHtml.setAttribute("data-theme", "light")
-    else rootHtml.setAttribute("data-theme", 'dark')
-    toggleTheme.classList.toggle("bi-sun")
-    toggleTheme.classList.toggle("bi-moon-stars")
+
+const swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: true,
+});
+
+
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  if (currentTheme === "dark") {
+    rootHtml.setAttribute("data-theme", "light");
+  } else {
+    rootHtml.setAttribute("data-theme", "dark");
+  }
+  
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
 }
 
-toggleTheme.addEventListener('click', changeTheme)
 
-accordionHeaders.forEach(header =>{
-    header.addEventListener("click", () =>{
-        const accordionItem = header.parentElement
-        const accordionActive = accordionItem.classList.contains("active")
+toggleTheme.addEventListener("click", changeTheme);
 
-        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
-    })
-})
 
-menuLinks.forEach(item => {
-    item.addEventListener("click", () => {
-      menuLinks.forEach(i => i.classList.remove("active"));
-      item.classList.add("active");
-    })
-  })
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    if (accordionActive) {
+      accordionItem.classList.remove("active");
+    } else {
+      accordionItem.classList.add("active");
+    }
+  });
+});
+
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
